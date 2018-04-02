63932c38560512a3def143c0d3e28ebc44460afc
warlordlizard <alexjbowers88@gmail.com>
2018-04-02 15:15:02 -0700

    app

--- lab-alex/frontend/src/components/app/index.js
+++ lab-alex/frontend/src/components/app/index.js
@@ -7,3 +7,19 @@ import appCreateStore from '../../lib/app-create-store.js';
 import Dashboard from '../dashboard';
 
 const store = appCreateStore();
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <main className='app'>
+        <Provider>
+          <BrowserRouter>
+            <section>
+              <Route exact path='/' component={Dashboard} />
+            </section>
+          </BrowserRouter>
+        </Provider>
+      </main>
+    );
+  }
+}
