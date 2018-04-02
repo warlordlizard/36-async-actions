1c7596ae7557217903c222c1c63d0a7a62911854
warlordlizard <alexjbowers88@gmail.com>
2018-04-02 14:24:26 -0700

    middleware

---
+++ lab-alex/frontend/src/lib/redux-reducer.js
@@ -0,0 +1,15 @@
+'use strict';
+
+cnst reporter = store => next => action => {
+  console.log('ACTION', action);
+  try {
+    let result = next(action);
+    console.log('__STATE IN REP__', store.getState());
+    return result;
+  } catch (error) {
+    error.action = action;
+    console.error('__ERROR__', error);
+    return error;
+  }
+}
+export default reporter;
