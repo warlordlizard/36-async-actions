6f0d1e7b05342e7688cc80fe4b2c7e881c117d9e
warlordlizard <alexjbowers88@gmail.com>
2018-04-02 14:14:29 -0700

    main.js

--- lab-alex/frontend/src/main.js
+++ lab-alex/frontend/src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import React from 'react';
+import App from './components/app';
+import ReactDom from 'react-dom';
 
-import ReactDom from 'r'
+ReactDom.render(<App />, document.getElementById('root'));
